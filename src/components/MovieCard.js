01631beb7445f75a2aa8ae79a563a5fac06894d1
yamila <yamilampalma@gmail.com>
2022-05-03 23:52:40 -0300

    Movies Grid

---
+++ src/components/MovieCard.js
@@ -0,0 +1,16 @@
+import styles from './MovieCard.module.css';
+export function MovieCard({ movie }) {
+	const imageUrl = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+
+	return (
+		<li className={styles.movieCard}>
+			<img
+				className={styles.movieImg}
+				height={345}
+				width={230}
+				src={imageUrl}
+				alt={movie.title}></img>
+			<div>{movie.title}</div>
+		</li>
+	);
+}
